45f1a3fefb6c9e45c2e696e8b381955413f14295
Saurabh Sheshrao Khandagale <59130610+Khandagale-Saurabh@users.noreply.github.com>
2022-01-31 20:13:04 +0530

    Update express.js

--- express.js
+++ express.js
@@ -1,3 +1,6 @@
 let express=require('express')
 let app=express();
-app.get('/Home',(req,res)=>{})
+app.get('/Home',(req,res)=>{
+we can write req.send() or res.json()
+  json will conver to json object and sendjson data
+})
