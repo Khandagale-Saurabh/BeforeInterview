c714b7a256b397b232cbed13467782010a9a5721
Saurabh Sheshrao Khandagale <59130610+Khandagale-Saurabh@users.noreply.github.com>
2022-02-18 19:58:01 +0530

    Update and rename express.js to JavaScrpit.js

---
+++ JavaScrpit.js
@@ -0,0 +1,16 @@
+check isuues
+lexical scpoing matlap top to down aur scpoe wise value change hoti hai
+======================================================
+impure function: affected by outter value
+let n=10;
+fun(a)
+{
+  return n+a
+}//depends on n so it is impure function
+
+pure function : it no affected by outter value
+
+fun(a,n)
+{
+  return n+a
+}// not depends on outter values so it is pure function
--- express.js
+++
@@ -1,6 +0,0 @@
-let express=require('express')
-let app=express();
-app.get('/Home',(req,res)=>{
-we can write req.send() or res.json()
-  json will conver to json object and sendjson data
-})
