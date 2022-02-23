662378a8a1cd23c4347d4b40a1039fbf0707bcc4
Saurabh Sheshrao Khandagale <59130610+Khandagale-Saurabh@users.noreply.github.com>
2022-02-24 00:49:31 +0530

    Update JavaScrpit.js

--- JavaScrpit.js
+++ JavaScrpit.js
@@ -18,3 +18,24 @@ fun(a,n)
 
 //PolyFill
 to create out own map,reducer and filter
+
+
+let movies=[{title: 'a', year:2018,rate:7.5},
+{title: 'b', year:2018,rate:4.5},
+{title: 'c', year:2020,rate:3},
+{title: 'd', year:2012,rate:5}
+]
+
+//movies with 2018 
+//sorted
+//decending rating
+//only title
+console.log(movies);
+let ans=movies.filter(m18=>{
+    return m18.year===2018
+}).sort((a,b)=>{
+    return a.rate -b.rate
+}).reverse()
+.map((g)=>{return g.title})
+
+console.log(ans);
